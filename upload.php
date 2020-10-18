b0a047f2b21354f2f98e2ad4cd5f1ed47b7eb093
JilaniMokrani <40571618+JilaniMokrani@users.noreply.github.com>
2020-10-18 17:48:34 +0100

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,9 +1,10 @@
 
 <?php
+        $target_dir = "/img/";
         $image = $_FILES["fileToUpload"];
-        
-	rename($image["tmp_name"],"/var/www/html/img/". $image["name"]);
-        shell_exec("aws s3 mv img/ s3://cloudjilaniensit/ --acl public-read --recursive");
+        $target_file = $target_dir . basename($image["name"]);
+        rename($image["tmp_name"],"/var/www/html/img/".$image["name"]);
+        system("aws s3 cp img/ s3://cloudjilaniensit/ --acl public-read --recursive");
 
         $servername = "pictures.cftgnvjnvxzz.us-east-1.rds.amazonaws.com";
         $username = "admin";
@@ -13,18 +14,10 @@
 
         $conn = new mysqli($servername, $username, $password, $database, $port);
 
-        if ($conn->connect_error) 
-        {
+        if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
         }
-        
+
         $query ="INSERT INTO Images (url,title) VALUES (\"" . basename($image["name"]) . "\", \"" . $_POST["title"] . "\");";
         $conn -> query($query);
-        
-        header("location: index.php");
 ?>
-~                                                                                                                                                                                
-~                                                                                                                                                                                
-~                                                                                                                                                                                
-~                                                                                                                                                                                
-~                                 
